6548e4c67e7c1835f621bcfdc0289a0858fcf56f
Tiagotj7 <tiagowork07@gmail.com>
2026-01-22 16:27:11 -0300

    adjust page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Head from 'next/head';
+import { Inter, Montserrat, Fira_Code } from 'next/font/google';
+import './globals.css';
+import { NavBar } from '../components/NavBar';
 
 export default function Page() {
   return (
-    <main className="min-h-screen"/>
+    
+    <NavBar />
+
   );
 }
