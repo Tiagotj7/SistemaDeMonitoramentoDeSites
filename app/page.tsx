866ffa138f3f4d98789191ea33083f7ff9fffcf7
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-01-22 15:30:15 -0300

    Update page.tsx
    -removendo a navbar do main

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import { Main as NavBar } from '../components/NavBar';
-
 export default function Page() {
   return (
-    <main className="min-h-screen">
-
-      <NavBar />
-
-    </main>
+    <main className="min-h-screen"/>
   );
 }
