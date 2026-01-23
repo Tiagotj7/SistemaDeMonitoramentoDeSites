ace121f70092a3d5384e6017d82f465e9c5d4f69
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-01-23 17:27:59 -0300

    Add Nav

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,11 +1,11 @@
 'use client';
 import Link from 'next/link';
-
+import styles from './NavBar.module.css';
 export const NavBar = () => {
   return (
-        <nav className="navbar">
-            <Link href="/" className="navbar-logo">MyApp</Link>
-            <menu className="navbar-links">
+        <nav className={styles.navbar}>
+            <Link href="/">MyApp</Link>
+            <menu>
                 <li><Link href="/about">About</Link></li>
                 <li><Link href="/services">Services</Link></li>
                 <li><Link href="/contact">Contact</Link></li>
