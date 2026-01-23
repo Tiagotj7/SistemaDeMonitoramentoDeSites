03ebf49dc32b0ff2419694df99b4453c6b80a48a
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-01-23 17:02:49 -0300

    debug
    -Resolvendo o bug do global.css não declarado

---
+++ app/src/layout.tsx
@@ -0,0 +1,18 @@
+// app/layout.tsx
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+import './global.css';
+
+export const metadata: Metadata = {
+  title: "Vigilant",
+  description:
+    "Plataforma de monitoramento em tempo real para aplicações web: ping, uptime, SSL, performance e analytics.",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
