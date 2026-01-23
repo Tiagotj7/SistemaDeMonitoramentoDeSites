c427bc5b0ee94c4be6d08122b9f9914370168e36
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-01-23 17:08:59 -0300

    Update NavBar.tsx

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,10 +1,9 @@
 'use client';
-import React from 'react';
 import Link from 'next/link';
 
 export const NavBar = () => {
   return (
-        <nav className="navbar  ">
+        <nav className="navbar">
             <Link href="/" className="navbar-logo">MyApp</Link>
             <menu className="navbar-links">
                 <li><Link href="/about">About</Link></li>
