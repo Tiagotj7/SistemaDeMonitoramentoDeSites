62e940fe24be0e7e2a4f59b49332f4f89a05ec37
Tiagotj7 <tiagowork07@gmail.com>
2026-01-23 19:45:25 -0300

    redirect page for home

---
+++ app/Home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Dashboard / Home</h1>
+      <p>Bem-vindo ao sistema Vigilant 🚀</p>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Loading } from '../components/Loading/Loading';
 import { NavBar } from '../components/NavBar/NavBar';
-import { Loading } from '@/components/Loading/Loading';
 
 export default function Page() {
-  return (
-    
-      <Loading />
+  const router = useRouter();
 
-  );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/home'); // destino
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return <Loading />;
 }
